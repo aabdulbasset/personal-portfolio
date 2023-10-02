f996f7f316c1223f6bd3d9442ef603784c6a5033
aabdulbasset <aabdulbasset5@gmail.com>
2023-10-02 17:34:52 +0300

    [fix] projects section animation now starts earlier

--- src/app/components/projectsSection.tsx
+++ src/app/components/projectsSection.tsx
@@ -1,28 +1,28 @@
-'use client';
+"use client";
 import { Flex, Grid, Heading, Text } from "@radix-ui/themes";
 import ProjectCard from "./ui/projectCard";
 import { SectionSeperator } from "./ui/sectionsSeperator";
 import { MotionFlex } from "./ui/motionFlex";
 
-export default  function Projects({projects}:{projects:Project[]}) {
-
+export default function Projects({ projects }: { projects: Project[] }) {
   return (
-    <MotionFlex direction={"column"} initial={{
+    <MotionFlex
+      direction={"column"}
+      initial={{
         translateY: 100,
         opacity: 0,
-    }}
-    whileInView={{
+      }}
+      whileInView={{
         translateY: 0,
         opacity: 1,
-    }}
-    transition={{
+      }}
+      transition={{
         duration: 1,
-    }}
-    viewport={{
+      }}
+      viewport={{
         once: true,
-        amount: 0.4,
-        
-    }}
+        amount: 0.25,
+      }}
     >
       <SectionSeperator sectionName={"Projects"} />
 
